e3925757ddabae26531d7f927a1c0555d5cd5a60
tken85 <terrykennair@Terrys-MacBook-Pro.local>
2015-12-04 11:18:50 -0500

    views and controller added

--- app/app.controllers.js
+++ app/app.controllers.js
@@ -13,8 +13,25 @@
       });
 
       vm.addPitch = function(newPitch){
-        PitchService.emit(newPitch);
+        PitchService.emit('new:pitch', newPitch);
+        $scope.newPitch = "";
       };
+      PitchService.on('new:pitch', function(data){
+        var pitch = {
+          title : data.title,
+          content : data.content,
+          needs : data.needs,
+          author: data.author,
+          comments: [],
+        };
+
+        $scope.pitches.push(pitch);
+
+      });
+
+      PitchService.on('all:pitches',function(data){
+        $scope.pitches= data;
+      });
 
       vm.addComment = function(pitch, comment){
         PitchService.addPitch(pitch,comment);
--- app/app.services.js
+++ app/app.services.js
@@ -6,13 +6,32 @@
     .factory('PitchService', function($http, _, $rootScope){
       var socket =io();
 
-      var addPitch = function(eventName, callback){
-        
-      }
+      var on = function(eventName, callback){
+        socket.on(eventName, function(){
+          var args = arguments;
+          console.log(args);
+          $rootScope.$apply(function(){
+            callback.apply(socket,args);
+          });
+        });
+      };
+
+      var emit = function(eventName, data, callback){
+        socket.emit(eventName, data, function(){
+          var args=arguments;
+          $rootScope.$apply(socket, function(){
+            if(callback){
+              callback.apply(socket.args);
+            }
+          });
+        });
+      };
 
       return{
         addPitch: addPitch,
-        addComment: addComment
+        addComment: addComment,
+        emit: emit,
+        on: on
       };
 
     });
